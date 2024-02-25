274ca5d6c0ef96b2328e15b5bf7222dbc4f0778f
sabin <sashre@utu.fi>
2024-02-25 17:31:46 +0200

    navbar is done

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
+import Navbar from "./Components/navbar/Navbar";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+};
 
 export default App;
